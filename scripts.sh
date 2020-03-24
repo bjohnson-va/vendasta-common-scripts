2e5ab2e017e0893cbf9859c51ae2f5526f6f95e7
Brad Johnson <bjohnson@vendasta.com>
2020-03-23 18:27:05 -0600

    SSC-1242 Move helper scripts to a place they can be reused

---
+++ scripts.sh
@@ -0,0 +1,26 @@
+function bigMessage {
+  echo -e "
+---------------------------------------------
+$1
+---------------------------------------------";
+}
+
+function promptForGCloud {
+  printf "Have you already run \"gcloud auth application-default login\"? [Y/n] ";
+  read runGC;
+  if [[ $runGC = "n" || $runGC = "N" ]]; then
+    bigMessage "Setting up gcloud auth.  This will open a browser."
+    read -p "Press [Enter] key to start..."
+    (set -x; gcloud auth application-default login);
+  fi
+}
+
+function promptForVendasta {
+  printf "Have you already run \"mscli auth login\"? [Y/n] ";
+  read runVA;
+  if [[ $runVA = "n" || $runVA = "N" ]]; then
+    bigMessage "Setting up Vendasta auth.  This will open a browser."
+    read -p "Press [Enter] key to start..."
+    (set -x; mscli auth login);
+  fi
+}
